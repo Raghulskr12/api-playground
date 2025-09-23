c3e58216920cc4041b42029cef5eeb39f2a87533
Raghulskr12 <kalairaghul70@gmail.com>
2025-09-23 21:02:44 +0530

    [UPDATE] Basic express server setup

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("Server is running on local host 5000")
+import express from 'express';
+
+const app = express();
+
+app.get('/',(req,res)=>
+{
+    res.send('welcome to the subscription tracker api!');
+}
+)
+
+
+app.listen(3000, 'localhost', ()=>{
+    console.log('server running port 3000');
+});
+export default app;
